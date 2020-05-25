75b1a273f4b4ddcd5e5c2577aa0fd8cd1a4ec9b1
dishant888 <52916900+dishant888@users.noreply.github.com>
2020-05-25 16:48:17 +0530

    25/5/2020

--- speech2.py
+++ speech2.py
@@ -27,3 +27,54 @@ def greet():
         speak('Good night')
 
 greet()
+
+import speech_recognition as sr
+
+def takeCmd():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening...')
+        r.adjust_for_ambient_noise(source,duration=1)
+        r.pause_threshold = 1 # Speaking speed
+        audio = r.listen(source)
+        print(audio)
+    try:
+        print('Recognizing...')
+        query = r.recognize_google(audio,language='en-IN')
+        print(f'User said: {query}\n')
+    except Exception as e:
+        print(e)
+        print('Speak Again')
+        return 'None'
+    return query
+
+var = takeCmd().lower()
+speak(var)
+
+import wikipedia
+import webbrowser
+import os
+
+if 'wikipedia' in var:
+    print('Im searching in wikipedia.....')
+    speak('Im searching in wikipedia')
+    var = var.replace('wikipedia','')
+    result = wikipedia.summary(var,sentences=2)
+    print(f'According to wikipedia {result}')
+    speak(f'According to wikipedia {result}')
+elif 'open youtube' in var:
+    print('Opening Youtube...')
+    speak('Opening Youtube...')
+    webbrowser.open('youtube.com')
+elif 'open google' in var:
+    print('Opening Google...')
+    speak('Opening Google...')
+    webbrowser.open('google.com')
+elif 'time' in var:
+    print(f'Time is: {datetime.datetime.now().strftime("%H:%M:%S")}')
+    speak(f'Time is: {datetime.datetime.now().strftime("%H:%M:%S")}')
+elif 'song' in var:
+    directory = 'E:\\songs\\'
+    songs = os.listdir(directory)
+    print(songs)
+    os.startfile(os.path.join(directory,songs[0]))
